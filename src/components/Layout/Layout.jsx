a1b88cbfe0ca478e709eed703f734f6b58a7c529
Jaroslaw Charchula <charchula@gmail.com>
2023-03-22 01:43:39 +0100

    add header html and css logic in Layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import css from './Layout.module.css';
+import { BiSearch } from 'react-icons/bi';
 
 const Layout = () => {
   return (
@@ -9,6 +11,20 @@ const Layout = () => {
           <div className='gradient-red'></div>
           <div className='gradient-orange'></div>
           <div className='gradient-blue'></div>
+        </div>
+        <div className={css.header}>
+          <span>{moment().format('dddd, Do MMM YYYY')}</span>
+          <div className={css.searchBar}>
+            <BiSearch size={20} />
+            <input type='text' placeholder='Search' />
+          </div>
+          <div className={css.profile}>
+            <img src='./profile.png' alt='user image' />
+            <div className={css.details}>
+              <span>Bob Sterk</span>
+              <span>bobsterk@gmail.com</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
