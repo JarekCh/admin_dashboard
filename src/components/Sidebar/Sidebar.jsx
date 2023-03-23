6e39510be86adbad2809f0b191c0ac8a88408688
Jaroslaw Charchula <charchula@gmail.com>
2023-03-23 23:43:51 +0100

    edir Sidebar css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,18 +12,18 @@ export const Sidebar = () => {
         <NavLink to='dashboard' className={css.item} title={'Dashboard'}>
           <MdSpaceDashboard size={30} />
         </NavLink>
+        <NavLink to='calendar' className={css.item} title='Calendar'>
+          <AiFillCalendar size={30} />
+        </NavLink>
+
+        <NavLink to='board' className={css.item} title='Trello Board'>
+          <FaTasks size={30} />
+        </NavLink>
+
+        <NavLink to='users' className={css.item} title='Users'>
+          <AiOutlineTable size={30} />
+        </NavLink>
       </div>
-      <NavLink to='calendar' className={css.item} title='Calendar'>
-        <AiFillCalendar size={30} />
-      </NavLink>
-
-      <NavLink to='board' className={css.item} title='Trello Board'>
-        <FaTasks size={30} />
-      </NavLink>
-
-      <NavLink to='users' className={css.item} title='Users'>
-        <AiOutlineTable size={30} />
-      </NavLink>
     </div>
   );
 };
