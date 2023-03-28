8d5184fd0610d51ca95bf218b80ca4fc7a19d409
Jaroslaw Charchula <charchula@gmail.com>
2023-03-29 00:18:43 +0200

    create StatisticsChart component and ccs module add StatisticsChart import to Statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,7 @@
 import css from './Statistics.module.css';
 import { BsArrowUpShort } from 'react-icons/bs';
 import { groupNumber } from '../../data';
+import { StatisticsChart } from '../StatisticsChart/StatisticsChart';
 
 const Statistics = () => {
   return (
@@ -34,6 +35,7 @@ const Statistics = () => {
           <span>$ {groupNumber(2000)}</span>
         </div>
       </div>
+      <StatisticsChart />
     </div>
   );
 };
---
+++ src/components/StatisticsChart/StatisticsChart.jsx
@@ -0,0 +1,3 @@
+export const StatisticsChart = () => {
+  return <div>StatisticsChart</div>;
+};
