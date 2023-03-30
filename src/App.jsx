ac480d64c2eefe370818d2140c3d94611e16688c
Jaroslaw Charchula <charchula@gmail.com>
2023-03-31 01:14:09 +0200

    add Calendar routing add Calendar jsx and css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Calendar from './pages/Calendar/Calendar';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route path='dashboard' element={<Dashboard />} />
+            <Route path='calendar' element={<Calendar />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Calendar/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import './Calendar.css';
+
+const Calendar = () => {
+  return <div>Calendar</div>;
+};
+
+export default Calendar;
