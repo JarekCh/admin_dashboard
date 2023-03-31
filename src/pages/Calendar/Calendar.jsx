dd2e252643ca063a83b4e6ebc0deb0b1a476f917
Jaroslaw Charchula <charchula@gmail.com>
2023-03-31 11:44:55 +0200

    add Calendar logic and css

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -5,8 +5,32 @@ import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import './Calendar.css';
 
+// TODO fix css
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className='calendar-container'>
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          allDaySlot={false}
+          initialView='timeGridWeek'
+          slotDuration={'01:00:00'}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={true}
+          nowIndicator={true}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
