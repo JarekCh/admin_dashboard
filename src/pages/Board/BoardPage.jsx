4902ed0c973814a68e97cea6e3252376e1e13d5c
Jaroslaw Charchula <charchula@gmail.com>
2023-04-02 01:12:57 +0200

    remove strict for kanban functionality add early styling edit BoardPage.jsx

--- src/pages/Board/BoardPage.jsx
+++ src/pages/Board/BoardPage.jsx
@@ -9,7 +9,32 @@ import useBoard from '../../store/Board';
 import './BoardPage.css';
 
 const BoardPage = () => {
-  return <div>Board</div>;
+  const { board, setBoard } = useBoard();
+
+  const handleColumnMove = (_card, source, destination) => {
+    const updatedBoard = moveColumn(board, source, destination);
+    setBoard(updatedBoard);
+  };
+
+  const handleCardMove = (_card, source, destination) => {
+    const updatedBoard = moveCard(board, source, destination);
+    setBoard(updatedBoard);
+  };
+
+  return (
+    <div className='board-container'>
+      <span>Trello Board</span>
+      <Board
+        allowAddColumn
+        allowRenameColumn
+        allowRemoveCard
+        onCardDragEnd={handleCardMove}
+        onColumnDragEnd={handleColumnMove}
+      >
+        {board}
+      </Board>
+    </div>
+  );
 };
 
 export default BoardPage;
