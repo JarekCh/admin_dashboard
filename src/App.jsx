5a08b5ba327a55fbb22adf26611bb226775ba8c2
Jaroslaw Charchula <charchula@gmail.com>
2023-04-01 23:15:32 +0200

    create BoardPage jsx and css file create store for Board set routing for BoardPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import BoardPage from './pages/Board/BoardPage';
 import Calendar from './pages/Calendar/Calendar';
 import Dashboard from './pages/Dashboard/Dashboard';
 
@@ -11,6 +12,7 @@ const App = () => {
           <Route path='/' element={<Layout />}>
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='calendar' element={<Calendar />} />
+            <Route path='board' element={<BoardPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Board/BoardPage.jsx
@@ -0,0 +1,15 @@
+import Board, {
+  moveCard,
+  moveColumn,
+  removeCard,
+  addCard,
+} from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
+import useBoard from '../../store/Board';
+import './BoardPage.css';
+
+const BoardPage = () => {
+  return <div>Board</div>;
+};
+
+export default BoardPage;
