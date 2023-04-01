0a776460414009b9ff8e1ce0cada76fd90ccb640
Jaroslaw Charchula <charchula@gmail.com>
2023-04-02 01:48:50 +0200

    add card styling edit board kanban add handleColumnMove, handleCardMove, getColumn, getGradient functions

--- src/pages/Board/BoardPage.jsx
+++ src/pages/Board/BoardPage.jsx
@@ -5,6 +5,8 @@ import Board, {
   addCard,
 } from '@asseinfo/react-kanban';
 import '@asseinfo/react-kanban/dist/styles.css';
+import { RxCross2 } from 'react-icons/rx';
+import { IoMdAdd } from 'react-icons/io';
 import useBoard from '../../store/Board';
 import './BoardPage.css';
 
@@ -21,6 +23,39 @@ const BoardPage = () => {
     setBoard(updatedBoard);
   };
 
+  const getColumn = (card) => {
+    const column = board.columns.filter((column) =>
+      column.cards.includes(card)
+    );
+    return column[0];
+  };
+
+  const getGradient = (card) => {
+    const column = getColumn(card);
+    const title = column.title;
+    if (title === 'TODO') {
+      return {
+        background:
+          'linear-gradient(65.35deg, rgba(65, 65, 65, 0.67) -1.72%, rgba(48, 189, 220) 163.54%)',
+      };
+    } else if (title === 'Doing') {
+      return {
+        background:
+          'linear-gradient(65.35deg, rgba(65, 65, 65, 0.67) -1.72%, rgba(220, 48, 48) 163.54%)',
+      };
+    } else if (title === 'Completed') {
+      return {
+        background:
+          'linear-gradient(65.35deg, rgba(65, 65, 65, 0.67) -1.72%, rgba(48, 220, 86) 163.54%)',
+      };
+    } else if (title === 'Backlog') {
+      return {
+        background:
+          'linear-gradient(65.35deg, rgba(65, 65,65, 0.67) -1.72%,rgba(134, 48, 220) 163.54%)',
+      };
+    }
+  };
+
   return (
     <div className='board-container'>
       <span>Trello Board</span>
@@ -30,6 +65,28 @@ const BoardPage = () => {
         allowRemoveCard
         onCardDragEnd={handleCardMove}
         onColumnDragEnd={handleColumnMove}
+        renderCard={(props) => (
+          <div className='kanban-card' style={getGradient(props)}>
+            <div>
+              <span>{props.title}</span>
+              <button
+                className='remove-button'
+                type='button'
+                onClick={() => {
+                  const updatedBoard = removeCard(
+                    board,
+                    getColumn(props),
+                    props
+                  );
+                  setBoard(updatedBoard);
+                }}
+              >
+                <RxCross2 color='white' size={15} />
+              </button>
+            </div>
+            <span>{props.description}</span>
+          </div>
+        )}
       >
         {board}
       </Board>
