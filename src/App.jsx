64d56237210f70879e5d80f290623dad66061a73
Jaroslaw Charchula <charchula@gmail.com>
2023-04-02 19:07:34 +0200

    add DataGrid jsx and css add routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout';
 import BoardPage from './pages/Board/BoardPage';
 import Calendar from './pages/Calendar/Calendar';
 import Dashboard from './pages/Dashboard/Dashboard';
+import DataGrid from './pages/DataGrid/DataGrid';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='calendar' element={<Calendar />} />
             <Route path='board' element={<BoardPage />} />
+            <Route path='users' element={<DataGrid />} />
           </Route>
         </Routes>
       </BrowserRouter>
